dff1854f8ac51dab76d7e3b99316bdd2b57147e3
Daffa Abipraya <daffaabhiprayaputra@gmail.com>
2024-07-05 20:27:29 +0700

    finished exercise 5.7

--- bloglist-frontend/src/components/Blog.jsx
+++ bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,33 @@
-const Blog = ({ blog }) => (
-	<div>
-		{blog.title} by {blog.author.name}
-	</div>
-);
+import { useState } from 'react';
+
+const Blog = ({ blog }) => {
+	const [detailView, setDetailView] = useState(false);
+
+	return (
+		<div className='blog'>
+			<div>
+				{blog.title} by{' '}
+				<span className='detail-span'>{blog.author.name}</span>
+				<button onClick={() => setDetailView(!detailView)}>view</button>
+			</div>
+			<div>
+				{detailView && (
+					<div>
+						<div>
+							<a href={blog.url}>{blog.url}</a>
+						</div>
+						<div>
+							<span className='detail-span'>
+								likes {blog.likes}
+							</span>
+							<button>like</button>
+						</div>
+						<div>{blog.author.name}</div>
+					</div>
+				)}
+			</div>
+		</div>
+	);
+};
 
 export default Blog;
